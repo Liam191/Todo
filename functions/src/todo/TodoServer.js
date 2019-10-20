5c86900ade529a1ed53585dd3876e4e509724e52
Liam Hilliard <liam.h.-@hotmail.com>
2019-10-20 21:49:34 +0100

    Add body parser dependency. Add tests for objects

--- functions/src/todo/TodoServer.js
+++ functions/src/todo/TodoServer.js
@@ -1,11 +1,20 @@
 "use strict";
 const express = require('express');
 const status = require('http-status-codes');
+const bodyparser = require('body-parser');
 
 function TodoServer(){
     const app = express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: true }));
+    
     app.all('*', (request, response) => {
-        return response.sendStatus(status.BAD_REQUEST);
+        if(request.body.name){
+            return response.sendStatus(status.CREATED);
+        } else {
+            return response.sendStatus(status.BAD_REQUEST);
+        }
     });
 
     return app;
--- functions/test/todo/postTodo.test.js
+++ functions/test/todo/postTodo.test.js
@@ -5,6 +5,10 @@ const status = require('http-status-codes');
 
 const { TodoServer } = require('../../src/todo/TodoServer');
 
+//TODO post named todo item
+//TODO get all todo items
+//TODO get one todo item
+
 describe('POST /todo', function(){
     let app;
     beforeEach(function(){
@@ -16,5 +20,19 @@ describe('POST /todo', function(){
             .post('/todo')
             .send() //Nothing
             .expect(status.BAD_REQUEST, done);
+    });
+
+    it('given an empty object, should return a 400 Bad Request code', function(done){
+        supertest(app)
+            .post('/todo')
+            .send({}) // Empty object
+            .expect(status.BAD_REQUEST, done);
+    });
+
+    it('given an object with a name, should return 201 Created', function(done){
+        supertest(app)
+            .post('/todo')
+            .send({ name: 'New Todo' })
+            .expect(status.CREATED, done);
     });
 });
